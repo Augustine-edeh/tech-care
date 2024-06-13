f70a8d84bbf8c9a3fa7f844dfa9cdaf2b2b8f4f5
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-06-13 10:03:26 +0100

    chore: add root style variables

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,9 @@
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header className="flex items-center h-[72px] bg-[var(--unnamed-color-ffffff)] rounded-[70px]">
+      Header
+    </header>
+  );
 };
 
 export default Header;
