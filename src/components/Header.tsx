e358ba5b0d2683757d68baa64b662806aed2f8a8
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-06-13 14:37:34 +0100

    chore: use custom color for `Header`

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import User from "./User";
 Image;
 const Header = () => {
   return (
-    <header className="flex items-center justify-between px-[33px] h-[72px] bg-[var(--unnamed-color-ffffff)] rounded-[70px]">
+    <header className="flex items-center justify-between px-[33px] h-[72px] bg-unnamed-color-ffffff rounded-[70px]">
       <Image src="/TestLogo.svg" alt="" width={211} height={48} />
       <Navbar />
       <User />
