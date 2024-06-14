2491ad8d89ec1512fa6fe796c2fed9a83bc3602d
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-06-14 09:48:01 +0100

    add `LabResults` component

---
+++ src/components/LabResults.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import React from "react";
+
+const LabResults = () => {
+  return (
+    <section className="bg-unnamed-color-ffffff w-[367px] h-[296px] rounded-[16px] p-5 mt-8">
+      <h3 className="font-manrope font-extrabold text-3xl leading-[33px] text-unnamed-color-072635">
+        Lab Results
+      </h3>
+
+      <ul className="mt-4">
+        <div className="flex justify-between">
+          <p className="font-manrope text-sm leading-[18px] text-unnamed-color-072635 text-left capitalize">
+            Blood Tests
+          </p>
+          <Image
+            src="/download_FILL0_wght300_GRAD0_opsz24 (1).svg"
+            width={20}
+            height={20}
+            alt="download result"
+          />
+        </div>
+      </ul>
+    </section>
+  );
+};
+
+export default LabResults;
--- src/components/PatientProfile.tsx
+++ src/components/PatientProfile.tsx
@@ -4,34 +4,39 @@ import PatientGender from "./PatientGender";
 import PatientContactInfo from "./PatientContactInfo";
 import PatientEmergencyContact from "./PatientEmergencyContact";
 import PatientInsuranceProvider from "./PatientInsuranceProvider";
+import LabResults from "./LabResults";
 
 const PatientProfile = () => {
   return (
-    <section className="bg-unnamed-color-ffffff w-[367px] h-[760px] rounded-[16px] px-5 pt-5 pb-5">
-      <div className="flex flex-col items-center gap-y-6 my-8">
-        <Image
-          src={`/Layer 2.png`}
-          width={200}
-          height={200}
-          alt={`Jessica Taylor`}
-        />
-        <h2 className="font-manrope font-extrabold text-3xl leading-[33px] text-unnamed-color-072635 text-center">{`Jessica Taylor`}</h2>
-      </div>
+    <div>
+      <section className="bg-unnamed-color-ffffff w-[367px] h-[760px] rounded-[16px] px-5 pt-5 pb-5">
+        <div className="flex flex-col items-center gap-y-6 my-8">
+          <Image
+            src={`/Layer 2.png`}
+            width={200}
+            height={200}
+            alt={`Jessica Taylor`}
+          />
+          <h3 className="font-manrope font-extrabold text-3xl leading-[33px] text-unnamed-color-072635 text-center">{`Jessica Taylor`}</h3>
+        </div>
 
-      <ul className="flex flex-col gap-y-6">
-        <PatientDOB />
-        <PatientGender />
-        <PatientContactInfo />
-        <PatientEmergencyContact />
-        <PatientInsuranceProvider />
-      </ul>
+        <ul className="flex flex-col gap-y-6">
+          <PatientDOB />
+          <PatientGender />
+          <PatientContactInfo />
+          <PatientEmergencyContact />
+          <PatientInsuranceProvider />
+        </ul>
 
-      <div className="grid place-items-center h-[63px] mt-[29px]">
-        <button className="w-[220px] h-[41px] rounded-[41px] bg-unnamed-activestate-bg-1 font-manrope font-bold text-base leading-[19px] text-unnamed-color-072635 ">
-          Show All Information
-        </button>
-      </div>
-    </section>
+        <div className="grid place-items-center h-[63px] mt-[29px]">
+          <button className="w-[220px] h-[41px] rounded-[41px] bg-unnamed-activestate-bg-1 font-manrope font-bold text-base leading-[19px] text-unnamed-color-072635 ">
+            Show All Information
+          </button>
+        </div>
+      </section>
+
+      <LabResults />
+    </div>
   );
 };
 
