dd8c123cd8d630d2c8b265e6aad894d8cdfb61d4
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-06-15 02:38:27 +0100

    chore(style): add styles to `Nav`

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,67 +2,92 @@ import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between w-[663px] h-[63px]">
-      <div className="flex gap-[9px] w-[88px] h-[19px]">
-        <Image
-          src="/home_FILL0_wght300_GRAD0_opsz24.svg"
-          width={16}
-          height={17}
-          alt="home"
-        />
+    <nav className="nav flex items-center justify-between w-[663px] h-[63px]">
+      <a
+        className="flex justify-center items-center gap-[9px] h-[41px]  focus:bg-unnamed-activestate-bg-1 hover:bg-unnamed-activestate-bg-2 outline-none rounded-[41px]"
+        href="#"
+      >
+        <div className="h-[19px] mx-5 flex justify-center gap-x-[9px]">
+          <Image
+            src="/home_FILL0_wght300_GRAD0_opsz24.svg"
+            width={16}
+            height={17}
+            alt="home"
+          />
 
-        <p className="font-manrope font-bold text-sm leading-[19px] text-unnamed-color-072635 text-left">
-          Overview
-        </p>
-      </div>
+          <p className="font-manrope font-bold text-sm leading-[19px] text-unnamed-color-072635 text-left">
+            Overview
+          </p>
+        </div>
+      </a>
 
-      <div className="flex gap-[9px] w-[88px] h-[19px]">
-        <Image
-          src="/group_FILL0_wght300_GRAD0_opsz24.svg"
-          width={24}
-          height={17}
-          alt="home"
-        />
-        <p className="font-manrope font-bold text-sm leading-[19px] text-unnamed-color-072635 text-left">
-          Patients
-        </p>
-      </div>
+      <a
+        className="flex justify-center items-center gap-[9px] h-[41px]  focus:bg-unnamed-activestate-bg-1 hover:bg-unnamed-activestate-bg-2 outline-none rounded-[41px]"
+        href="#"
+      >
+        <div className="h-[19px] mx-5 flex justify-center gap-x-[9px]">
+          <Image
+            src="/group_FILL0_wght300_GRAD0_opsz24.svg"
+            width={24}
+            height={17}
+            alt="home"
+          />
+          <p className="font-manrope font-bold text-sm leading-[19px] text-unnamed-color-072635 text-left">
+            Patients
+          </p>
+        </div>
+      </a>
 
-      <div className="flex gap-[9px] w-[88px] h-[19px]">
-        <Image
-          src="/calendar_today_FILL0_wght300_GRAD0_opsz24.svg"
-          width={24}
-          height={17}
-          alt="home"
-        />
-        <p className="font-manrope font-bold text-sm leading-[19px] text-unnamed-color-072635 text-left">
-          Schedule
-        </p>
-      </div>
+      <a
+        className="flex justify-center items-center gap-[9px] h-[41px]  focus:bg-unnamed-activestate-bg-1 hover:bg-unnamed-activestate-bg-2 outline-none rounded-[41px]"
+        href="#"
+      >
+        <div className="h-[19px] mx-5 flex justify-center gap-x-[9px]">
+          <Image
+            src="/calendar_today_FILL0_wght300_GRAD0_opsz24.svg"
+            width={15}
+            height={17}
+            alt="home"
+          />
+          <p className="font-manrope font-bold text-sm leading-[19px] text-unnamed-color-072635 text-left">
+            Schedule
+          </p>
+        </div>
+      </a>
 
-      <div className="flex gap-[9px] w-[88px] h-[19px]">
-        <Image
-          src="/group_FILL0_wght300_GRAD0_opsz24.svg"
-          width={24}
-          height={17}
-          alt="home"
-        />
-        <p className="font-manrope font-bold text-sm leading-[19px] text-unnamed-color-072635 text-left">
-          Message
-        </p>
-      </div>
+      <a
+        className="flex justify-center items-center gap-[9px] h-[41px]  focus:bg-unnamed-activestate-bg-1 hover:bg-unnamed-activestate-bg-2 outline-none rounded-[41px]"
+        href="#"
+      >
+        <div className="h-[19px] mx-5 flex justify-center gap-x-[9px]">
+          <Image
+            src="/group_FILL0_wght300_GRAD0_opsz24.svg"
+            width={19}
+            height={17}
+            alt="home"
+          />
+          <p className="font-manrope font-bold text-sm leading-[19px] text-unnamed-color-072635 text-left">
+            Message
+          </p>
+        </div>
+      </a>
 
-      <div className="flex gap-[9px] w-[119px] h-[19px]">
-        <Image
-          src="/credit_card_FILL0_wght300_GRAD0_opsz24.svg"
-          width={24}
-          height={17}
-          alt="home"
-        />
-        <p className="font-manrope font-bold text-sm leading-[19px] text-unnamed-color-072635 text-left">
-          Transactions
-        </p>
-      </div>
+      <a
+        className="flex justify-center items-center gap-[9px] h-[41px]  focus:bg-unnamed-activestate-bg-1 hover:bg-unnamed-activestate-bg-2 outline-none rounded-[41px]"
+        href="#"
+      >
+        <div className="h-[19px] mx-5 flex justify-center gap-x-[9px]">
+          <Image
+            src="/credit_card_FILL0_wght300_GRAD0_opsz24.svg"
+            width={22}
+            height={17}
+            alt="home"
+          />
+          <p className="font-manrope font-bold text-sm leading-[19px] text-unnamed-color-072635 text-left">
+            Transactions
+          </p>
+        </div>
+      </a>
     </nav>
   );
 };
